6ba31108500aabac7b5c20818991a9c412087759
Manish Sahu <manissh7352@gmail.com>
2021-12-03 22:45:44 +0530

    added various functionalities to the api

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,38 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async errors");
-  res.status(200).json({ msg: "testing proudcts" });
+  const products = await Product.find({}).sort("-name price");
+  res.status(200).json({ products, ngHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "not static proudcts" });
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const products = await result;
+
+  res.status(200).json({ ngHits: products.length, products });
 };
 
 module.exports = {
